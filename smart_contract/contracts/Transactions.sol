9c3385596125810bcbc9f93d516f69e941c9382b
GIORGI <kechekhmadzegiorgi@gmail.com>
2022-05-10 13:29:35 +0400

    first commit

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transcations {
+    uint256 transactionsCount;
+
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint256 amount;
+        string message;
+        string keyword;
+        uint256 timestamp;
+    }
+
+    TransferStruct[] transcations;
+
+    function addToBlockchain() public {
+        transactionsCount += 1;
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {}
+
+    function getTransactionCount() public view returns (uint256) {
+        
+    }
+}
